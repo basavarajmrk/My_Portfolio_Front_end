199f4a91e32e22296d820a298395a63f6b002c42
basavaraj <basava.mrk@gmail.com>
2025-06-07 17:32:25 +0530

    test

--- portfolio/src/axiosInstance.js
+++ portfolio/src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_BASE || "https://basavarajportfolio.netlify.app/";
+const API_BASE = process.env.REACT_APP_API_BASE || "https://myportfolio-304v.onrender.com/api";
 
 const api = axios.create({
     baseURL: API_BASE,

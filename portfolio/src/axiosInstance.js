ad751d8196f4164700e3a649d735e2082dd8e978
basavaraj <basava.mrk@gmail.com>
2025-06-07 17:24:58 +0530

    test121

--- portfolio/src/axiosInstance.js
+++ portfolio/src/axiosInstance.js
@@ -1,14 +1,13 @@
-
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:8000/";
+const API_BASE = process.env.REACT_APP_API_BASE || "https://basavarajportfolio.netlify.app/";
 
 const api = axios.create({
     baseURL: API_BASE,
     timeout: 5000,
     headers: {
-      'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
     },
-  });
-  
-  export default api;
+});
+
+export default api;
